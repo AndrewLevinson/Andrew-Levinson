cd7acb31e83c7d5277135ea00cbc89d43896290d
andrew_l <andrewlevins@gmail.com>
2018-08-11 12:09:07 -0400

    update next project links, change small type style, and change fix js console errors

--- scripts/projects.js
+++ scripts/projects.js
@@ -67,14 +67,3 @@ $(document).ready(function() {
     }
   });
 });
-
-// slick image carousel
-$(document).ready(function() {
-  $(".carousel").slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
-});
